e63e08fee0908f47d5a03c0f8cc79794c712befe
Eyal <rabeyal123@gmail.com>
2022-01-12 16:24:09 +0200

    update the background

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll('.color h2');
 let initialColors;
 
+//Add event listeners
+sliders.forEach(slider => {
+    slider.addEventListener("input", hslControls);
+})
+
+
+
+
 //Functions 
 
 //Color Generator
@@ -66,6 +74,25 @@ function colorizeSliders(color, hue, brightness, saturation){
     brightness.style.backgroundImage = `linear-gradient(to right,${scaleBright(0)}
     , ${scaleBright(0.5)}, ${scaleBright(1)})`;
     hue.style.backgroundImage = 'linear-gradient(to right, rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))';
+}
+
+function hslControls(e){
+    const index = 
+        e.target.getAttribute('data-hue') || 
+        e.target.getAttribute('data-bright') || 
+        e.target.getAttribute('data-sat')
+    
+    let sliders = e.target.parentElement.querySelectorAll('input');
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    const bgColor = colorDivs[index].querySelector('h2').innerText;
+    let color = chroma(bgColor)
+        .set('hsl.s', saturation.value)
+        .set('hsl.l', brightness.value)
+        .set('hsl.h', hue.value);
+    colorDivs[index].style.backgroundColor = color;
 }
 
 
