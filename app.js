2f87f33a7d8cb4f26c9545529b6d55561d86bba3
Eyal <rabeyal123@gmail.com>
2022-01-12 15:29:16 +0200

    coloring our inputs

--- app.js
+++ app.js
@@ -32,6 +32,13 @@ function randomColors(){
         hexText.innerText = randomColor;
         //Check for contrast
         checkTextContrast(randomColor, hexText);
+        //Initial Colorize sliders
+        const color = chroma(randomColor);
+        const sliders = div.querySelectorAll(".sliders input");
+        const hue = sliders[0];
+        const brightness = sliders[1];
+        const saturation = sliders[2];
+        colorizeSliders(color, hue, brightness, saturation);
     })
 }
 
@@ -42,7 +49,24 @@ function checkTextContrast(color,text){
     } else{
         text.style.color = "white";
     }
-
 }
+
+function colorizeSliders(color, hue, brightness, saturation){
+    //Scale Saturation
+    const noSat = color.set('hsl.s', 0); //reduce saturation to minimum 
+    const fullSat = color.set('hsl.s', 1); //increase saturation to minimum 
+    const scaleSat = chroma.scale([noSat, color, fullSat]);
+    //Scale Brightness
+    const midBright = color.set('hsl.l', 0.5);
+    const scaleBright = chroma.scale(['black', midBright, 'white']);
+
+    //Update Input colors
+    saturation.style.backgroundImage = `linear-gradient(to right,${scaleSat(0)}
+    , ${scaleSat(1)})`;
+    brightness.style.backgroundImage = `linear-gradient(to right,${scaleBright(0)}
+    , ${scaleBright(0.5)}, ${scaleBright(1)})`;
+    hue.style.backgroundImage = 'linear-gradient(to right, rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))';
+}
+
 
 randomColors();
