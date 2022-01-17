fd2ebafcbea81ed45a8bfd90878ff4dcd8ba6f4e
Eyal <rabeyal123@gmail.com>
2022-01-17 16:01:13 +0200

    open close adjustment

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const generateBtn = document.querySelector('.generate');
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll('.color h2');
 const popup = document.querySelector('.copy-container');
+const adjustButton = document.querySelectorAll('.adjust');
+const closeAdjustments = document.querySelectorAll('.close-adjustment');
+const sliderContainers = document.querySelectorAll('.sliders');
 let initialColors;
 
 //Add event listeners
@@ -24,6 +27,16 @@ popup.addEventListener('transitionend', () =>{
     const popupBox = popup.children[0];
     popup.classList.remove('active');
     popupBox.classList.remove('active');
+})
+adjustButton.forEach((button,index) => {
+    button.addEventListener('click',()=>{
+        openAdjustmentPanel(index);
+    });
+})
+closeAdjustments.forEach((button,index) => {
+    button.addEventListener('click',()=>{
+        openAdjustmentPanel(index);
+    });
 })
 
 
@@ -163,6 +176,12 @@ function copyToClipboard(hex){
     console.log(popupBox);
     popup.classList.add('active');
     popupBox.classList.add('active');
+}
+function openAdjustmentPanel(index){
+    sliderContainers[index].classList.toggle('active');
+}
+function closeAdjustmentPanel(index){
+    sliderContainers[index].classList.remove('active');
 }
 
 randomColors();
