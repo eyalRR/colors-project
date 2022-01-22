dcc25e967e053132fd513a1e4d839ff5dc8ded1f
Eyal <rabeyal123@gmail.com>
2022-01-22 17:06:02 +0200

    update library from local storage

--- app.js
+++ app.js
@@ -251,7 +251,13 @@ function savePalette(e){
         colors.push(hex.innerText);
     });
     //Generate object
-    let paletteNr = savedPalettes.length;
+    let paletteNr;
+    const paletteObjects = JSON.parse(localStorage.getItem('paletteObjects'));
+    if(paletteObjects){
+        paletteNr = paletteObjects.length;
+    } else{
+        paletteNr = savedPalettes.length;
+    }
     const paletteObj = {name, colors, nr:paletteNr};
     savedPalettes.push(paletteObj);
     //Save to LocalStorage
@@ -315,7 +321,55 @@ function closeLibrary(){
     libraryContainer.classList.remove('active');
     popup.classList.remove('active');
 }
+function getLocal(){
+    if(localStorage.getItem('palettes') === null){
+        localPalettes = [];
+    } else{
+        const paletteObjects = JSON.parse(localStorage.getItem("palettes"));
 
+        savedPalettes = [...paletteObjects];
+        paletteObjects.forEach(paletteObj => {
+            //Generate the palette for Library
+            const palette = document.createElement('div');
+            palette.classList.add('custom-palette');
+            const title = document.createElement('h4');
+            title.innerText = paletteObj.name;
+            const preview = document.createElement('div');
+            preview.classList.add('small-preview');
+            paletteObj.colors.forEach(smallColor => {
+                const smallDiv = document.createElement('div');
+                smallDiv.style.background = smallColor;
+                preview.appendChild(smallDiv);
+            });
+            const paletteBtn = document.createElement('button');
+            paletteBtn.classList.add('pick-palette-btn');
+            paletteBtn.classList.add(paletteObj.nr);
+            paletteBtn.innerText = 'Select';
+        
+            //Attach event to the buttons
+            paletteBtn.addEventListener('click', e => {
+                closeLibrary();
+                const paletteIndex = e.target.classList[1];
+                initialColors = [];
+                paletteObjects[paletteIndex].colors.forEach((color, index) => {
+                    initialColors.push(color);
+                    colorDivs[index].style.backgroundColor = color;
+                    const text = colorDivs[index].children[0];
+                    checkTextContrast(color, text);
+                    updateTextUI(index);
+                });
+                resetInputs();
+            })
+        
+            //Appent to library 
+            palette.appendChild(title);
+            palette.appendChild(preview);
+            palette.appendChild(paletteBtn);
+            libraryContainer.children[0].appendChild(palette);
+        })
+    }
+}
 
-
+//localStorage.clear();
+getLocal();
 randomColors();
